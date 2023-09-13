8a3f4bc533b8347cb06715224260cc51a1527bd3
lukitas <zarox423@gmail.com>
2023-09-13 14:31:19 +0200

    File1Mostrando

--- HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
+++ HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -15,7 +15,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         File ficheroPrueba = new File(".\\ejemploprueba.txt");
-        System.out.println(ficheroPrueba.getPath());
         float tamano = ficheroPrueba.length();
         System.out.println(tamano + " bytes");
     }    
