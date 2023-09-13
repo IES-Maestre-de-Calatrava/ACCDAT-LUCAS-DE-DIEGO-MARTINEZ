033b7778a75f9c4157e707ac39c5f9b9854201d4
lukitas <zarox423@gmail.com>
2023-09-13 13:59:58 +0200

    ruta relativa arreglada

--- HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
+++ HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -14,7 +14,8 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        File ficheroPrueba = new File("L:\\NetBeans Projects\\ACCDAT-LUCAS-DE-DIEGO-MARTINEZ\\HolaMundo\\src\\ejemploprueba.txt");
+        File ficheroPrueba = new File(".\\ejemploprueba.txt");
+        System.out.println(ficheroPrueba.getPath());
         float tamano = ficheroPrueba.length();
         System.out.println(tamano + " bytes");
     }    
